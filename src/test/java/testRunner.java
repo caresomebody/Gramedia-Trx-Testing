f6a1ac38ca56651ec0d0be5dc87dace93e2a66b3
Ali Naufal Ammarullah <48080443+caresomebody@users.noreply.github.com>
2023-08-05 22:37:42 +0700

    add serenity

--- src/test/java/testRunner.java
+++ src/test/java/testRunner.java
@@ -1,11 +1,14 @@
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = "src/test/Feature"
-        ,glue={"src/test/StepDefinition"}
+        features = "src/test/resources/Feature",
+        plugin = {"pretty"},
+        tags = "not @skip",
+        glue={"StepDefinition"}
 )
 
 public class testRunner {
